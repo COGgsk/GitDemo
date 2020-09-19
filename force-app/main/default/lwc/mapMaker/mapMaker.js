8856a1cfff62fde358bb9683dd5830e6a5da0677
ROYHeroku <69654399+ROYHeroku@users.noreply.github.com>
2020-09-20 04:50:59 +0530

    GitDemo Commit 1

---
+++ force-app/main/default/lwc/mapMaker/mapMaker.js
@@ -0,0 +1,31 @@
+import { LightningElement, api, track } from 'lwc';
+
+export default class MapMaker extends LightningElement {
+    @api Latitude;  
+    @api Longitude;
+    @track InputValues = {
+        latitude:  this.Lat,
+        longitude: this.Long
+      };    
+    
+    get Lat() {  
+        return this.Latitude;  
+      }  
+    set Lat(value) {  
+        this.Latitude = value;  
+      } 
+    get Long() {  
+        return this.Longitude;  
+      }  
+    set Long(value) {  
+        this.Longitude = value;  
+      }  
+    showPosition(position) {    
+      this.Lat = position.coords.latitude; 
+      this.Long = position.coords.longitude; 
+
+    }
+    renderedCallback() {
+        this.dataJson = JSON.stringify(this.InputValues);
+      }
+}
